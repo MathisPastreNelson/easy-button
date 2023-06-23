8867a5689da6048ea0f7fac5cfe336079b4bbfd7
MathisP <103594397+MathisPastreNelson@users.noreply.github.com>
2023-06-23 18:42:04 +0200

    test

--- components/Dropdown1.vue
+++ components/Dropdown1.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="dropdown1_Container">
     <button class="dropdown1">Dropdown example 1</button>
-    <div>test</div>
+    <ul>
+      <li>Test</li>
+      <li>Test</li>
+      <li>Test</li>
+      <li>Test</li>
+    </ul>
   </div>
 </template>
