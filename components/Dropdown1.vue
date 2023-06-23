aaf463a99d3f3bcad6c4770d411bc622f6ebe64d
MathisP <103594397+MathisPastreNelson@users.noreply.github.com>
2023-06-23 19:02:34 +0200

    gh good test

--- components/Dropdown1.vue
+++ components/Dropdown1.vue
@@ -2,7 +2,7 @@
   <div class="dropdown1_Container">
     <button class="dropdown1">Dropdown example 1</button>
     <ul>
-      <li>Test</li>
+      <li>Ceci est le bon test</li>
       <li>Test</li>
       <li>Test</li>
       <li>Test</li>
