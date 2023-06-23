85c4624355f863ef2c596eeff47039ea50a43413
MathisP <103594397+MathisPastreNelson@users.noreply.github.com>
2023-06-23 19:06:59 +0200

    gh page ok !   npm run deploy > push sur ma ghpage

--- components/Dropdown1.vue
+++ components/Dropdown1.vue
@@ -2,8 +2,8 @@
   <div class="dropdown1_Container">
     <button class="dropdown1">Dropdown example 1</button>
     <ul>
-      <li>Ceci est le bon test</li>
-      <li>Test</li>
+      <li>Ceci est le bon test !</li>
+      <li>oh oui le bon !m!!</li>
       <li>Test</li>
       <li>Test</li>
     </ul>
